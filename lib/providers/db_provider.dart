fda7bf65967187a6cd200abb4ea140f2df0204d4
Matias Báez <matiasbaez2512@gmail.com>
2022-10-16 13:00:08 -0300

    Added, sqlite provider

---
+++ lib/providers/db_provider.dart
@@ -0,0 +1,118 @@
+
+import 'dart:io';
+import 'package:path/path.dart';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'package:qr_reader/models/models.dart';
+
+class DBProvider {
+
+  static Database? _database;
+  static final DBProvider db = DBProvider._();
+
+  DBProvider._();
+
+  get database async {
+    if (_database != null) return _database;
+
+    _database = await initDB();
+    return _database;
+  }
+
+  Future<Database> initDB() async {
+    Directory documentsDir = await getApplicationDocumentsDirectory();
+    final path = join( documentsDir.path, 'ScansDB.db' );
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE scans(
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            value TEXT
+          );
+        ''');
+      }
+    );
+  }
+
+  Future<int> newRawScan( ScanModel scan ) async {
+    final id    = scan.id;
+    final type  = scan.type;
+    final value = scan.value;
+
+    final db = await database;
+
+    final result = await db.insertRaw('''
+      INSERT INTO scans( id, type, value )
+      VALUES ( $id, '$type', '$value'  );
+    ''');
+
+    return result;
+  }
+
+  Future<int> newScan( ScanModel scan ) async {
+    final db = await database;
+
+    final result = await db.insert('scans', scan.toJson());
+
+    return result;
+  }
+
+  Future<ScanModel?> getScanById( int id ) async {
+    final db = await database;
+
+    final result = await db.query('scans', where: 'id=?', whereArgs: [id]);
+
+    return result.isNotEmpty ? ScanModel.fromJson(result.first) : null;
+  }
+
+  Future<List<ScanModel>> getAllScans( ) async {
+    final db = await database;
+
+    final results = await db.query('scans');
+
+    return results.isNotEmpty
+      ? results.map((r) => ScanModel.fromJson(r)).toList()
+      : [];
+  }
+
+  Future<List<ScanModel>> getScanByType( String type ) async {
+    final db = await database;
+
+    final results = await db.query('scans', where: 'type=?', whereArgs: [type]);
+
+    return results.isNotEmpty
+      ? results.map((r) => ScanModel.fromJson(r)).toList()
+      : [];
+  }
+
+  Future<int> updateScan( ScanModel scan ) async {
+    final db = await database;
+
+    final result = await db.update('scans', scan.toJson(), where: 'id=?', whereArgs: [scan.id]);
+
+    return result;
+  }
+
+  Future<int> deleteScan( ScanModel id ) async {
+    final db = await database;
+
+    final result = await db.delete('scans', where: 'id=?', whereArgs: [id]);
+
+    return result;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db = await database;
+
+    final result = await db.delete('scans');
+
+    return result;
+  }
+
+}
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,2 +1,3 @@
 
+export 'package:qr_reader/providers/db_provider.dart';
 export 'package:qr_reader/providers/ui_provider.dart';
